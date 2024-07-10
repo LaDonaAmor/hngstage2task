e927f803dd76a12e11bfe9bc749b158e18831d3b
Racheal Ifeoluwa Ogunmodede <razylove02@gmail.com>
2024-07-10 07:31:43 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-document.title = 'Timbu Cloud Store';
-// Other metadata settings
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
